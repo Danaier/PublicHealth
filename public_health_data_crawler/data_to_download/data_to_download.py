c0541599df8ea0e3d6bc802d75ae46cf23ed0d2b
aier <danqingaier@gmail.com>
2024-04-15 14:49:26 +0800

    feat:完成数据申请模块

--- public_health_data_crawler/data_to_download/data_to_download.py
+++ public_health_data_crawler/data_to_download/data_to_download.py
@@ -1,7 +1,4 @@
-import json
-import time
-from collections import defaultdict
-
+from public_health_data_crawler.data_to_download.apply_for_data import apply_for_data
 from public_health_data_crawler.selenium_adapter.HiddenPrints import HiddenPrints
 import public_health_data_crawler.selenium_adapter.Page
 from public_health_data_crawler.selenium_adapter.element_position import *
@@ -13,33 +10,9 @@ page = public_health_data_crawler.selenium_adapter.Page.Page()
 page.open_page(data_query_url)
 login_to_phsdc(page)
 
-# 2.获取已申请数据集
-with open("../resources/applied_data.json", 'r', encoding='utf-8') as applied_data_file:
-    applied_data_dict = defaultdict(list, json.load(applied_data_file))
-
 # 3.打开个人中心并申请数据
-page.open_page(data_download_url)  # 重新打开页面
-time.sleep(2)
-click(page.find_element_by_id(data_to_apply_list_btn_id))  # 点击“拟申请列表”
-current_page = max(int(key) for key in applied_data_dict.keys())  # 获取已申请进度
-page.find_element_by_id(page_num_input_id).send_keys(str(current_page))  # 输入页数
-click([item for item in page.find_li_elements_by_parent_id(page_num_table_id)
-       if text(item) == "跳转"][0])  # 点击“跳转”
-paged_apply_list = page.find_li_elements_by_parent_id(paged_apply_list_id)  # 获取当前页的数据集
-for paged_apply_data in paged_apply_list:
-    paged_apply_data_name = text(paged_apply_data)
-    if paged_apply_data_name not in applied_data_dict[str(current_page)]:
-        # 将未加入申请列表的数据库名称加入申请列表
-        click(paged_apply_data.find_element("tag name", "input"))  # 点击申请数据
-        click(page.find_element_by_id(apply_accept_clause_btn_id))  # 勾选“同意条款”
-        click(page.find_element_by_id(apply_data_btn_id))  # 点击“申请”
-
-        # 更新已申请进度
-        applied_data_dict[str(current_page)].append(paged_apply_data_name)
-        with open("../resources/applied_data.json", 'w', encoding='utf-8') as applied_data_file:
-            json.dump(applied_data_dict, applied_data_file, ensure_ascii=False)
-        break
-
+while apply_for_data(page):
+    pass
 
 time.sleep(6)
 page.close_page()
--- public_health_data_crawler/selenium_adapter/element_position.py
+++ public_health_data_crawler/selenium_adapter/element_position.py
@@ -1,25 +1,39 @@
 # 控件定位
 data_query_url = "https://www.phsciencedata.cn/Share/edtShareNew.jsp"
 data_download_url = "https://www.phsciencedata.cn/Share/jsp/share/ky_usersharenew.jsp"
+
+# 登录页面
 login_btn_xpath = "//*[@id=\"li1\"]/a[1]"
 user_center_btn_xpath = "//*[@id=\"navul\"]/li[8]/a[1]"
 username_input_id = "tx_userName"
 password_input_id = "tx_password"
-more_classification_btn_class_name = "checkClass12"
-classification_name_btn_class_name1 = "checkClass1"
-classification_name_btn_class_name2 = "checkClass11"
 code_img_id = "tokenImage"
 code_img_input_id = "tx_tokenValue"
 login_btn_id = "btn_logon"  # 真逆天吧，这登录是logon，写这个代码的不用review吗？
 refresh_img_xpath = '//*[@id="mima"]/div[6]/span[2]/img[2]'
+
+# 数据加入拟申请列表页面
+more_classification_btn_class_name = "checkClass12"
+classification_name_btn_class_name1 = "checkClass1"
+classification_name_btn_class_name2 = "checkClass11"
 search_text_input_id = "searchText"
 search_btn_id = "searchList"
 add_to_apply_list_btns_class = "btn-jiaru"
 
+# 个人中心页面
 data_to_apply_list_btn_id = "show1"
 page_num_input_id = "textnum"
 page_num_table_id = "fenye"
 paged_apply_list_id = "intendApplyData"
 apply_accept_clause_btn_id = "sqtype"
 apply_data_btn_id = "inBtn"
+
+# 申请数据页面
+serve_object_type_id = "serviceObj"
+serve_object_name_id = "serviceOrg"
+data_usage_id = "item_type"
+data_usage_description_id = "text_content"
+paper_title_input_id = "paptitle"
+submit_apply_btn_id = "addBtn"
+confirm_submit_btn_id = "addsubmitBtn"
 
