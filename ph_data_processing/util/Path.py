8d4be93945fd64e55344b07396b95d946e346107
aier <danqingaier@gmail.com>
2024-04-17 17:29:14 +0800

    fix:增强鲁棒性

--- ph_data_processing/util/Path.py
+++
@@ -1,6 +0,0 @@
-from ph_data_processing.util.root_path import root_path
-
-
-print(root_path/'resources')
-
-
--- ph_data_processing/util/root_path.py
+++
@@ -1,5 +0,0 @@
-import pathlib
-
-# 获取当前文件所在目录
-root_path = pathlib.Path(__file__).parent.parent
-
