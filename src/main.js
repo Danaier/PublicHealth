6ddbb10bfdd9b42373a45503824c2ccdb4e047ff
aier <danqingaier@gmail.com>
2024-04-25 09:14:34 +0800

    feat:设置element-plus语言为中文

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import './style.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
 
