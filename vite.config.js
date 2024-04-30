4806bd533f079bd3c672bc595b95c07dd2ce412d
aier <danqingaier@gmail.com>
2024-04-30 21:59:50 +0800

    feat:通过不同运行环境加载不同后端配置

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {inject} from "vue";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
- },
-})
+
+export default ({mode}) => {
+
+    const env = loadEnv(mode, process.cwd());
+
+    return defineConfig({
+        plugins: [vue()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
+            }
+        },
+    })
+
+}
+
+
+
